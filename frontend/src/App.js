ceb7fb7f203ed94cfa9af55d7a2075a4c35677e0
Raju760732 <pratapbunty123@gmail.com>
2025-11-16 19:24:12 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import AdminLogin from "./AdminLogin";
+import Dashboard from "./Dashboard";
 
 export default function App() {
-  return <AdminLogin />;
+  const token = localStorage.getItem("token");
+
+  // If token does not exist → show login page
+  if (!token) {
+    return <AdminLogin />;
+  }
+
+  // If token exists → show dashboard
+  return <Dashboard />;
 }
